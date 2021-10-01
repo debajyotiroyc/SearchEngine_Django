244e2f2d46fdd1335cb08f8d6047cec9308248f3
Debajyoti_Roy_Chowdhury <63442163+debajyotiroyc@users.noreply.github.com>
2021-10-01 19:32:30 +0530

    Add files via upload

--- search/views.py
+++ search/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
+def search(request):
+    if request.method == "POST":
+        search=request.POST['search']
+    return render(request,"index.html")
