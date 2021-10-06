fc08f2a32b4d7bdfc79e3a7697171a3708d1e6a8
Debajyoti_Roy_Chowdhury <63442163+debajyotiroyc@users.noreply.github.com>
2021-10-06 11:47:09 +0530

    Add files via upload

--- search/views.py
+++ search/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+import requests
+from bs4 import BeautifulSoup as bs
 # Create your views here.
 def index(request):
     return render(request,"index.html")
@@ -7,4 +8,7 @@ def index(request):
 def search(request):
     if request.method == "POST":
         search=request.POST['search']
+        url='https://www.ask.com/web?q='+search
+        res = requests.get(url)
+        soup = bs(res.text,'lxml')
     return render(request,"index.html")
